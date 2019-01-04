5941cb2c256fe8c3b8a4538ff11d2745578302c0
Harshil Jhaveri <36830810+harshiljhaveri@users.noreply.github.com>
2019-01-04 22:57:26 +0530

    Update models.py

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,12 +1,7 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
 
 class newslet(models.Model):
     email=models.EmailField(max_length=254,**options)
     
-    def __str__(self):
-        return self.email
 
 
