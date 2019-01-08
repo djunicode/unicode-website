46da426a90bb65c45f852692889aefc3d618cd98
sagarbagwe6699 <sagarbagwe6699@gmail.com>
2019-01-08 18:56:23 +0530

    InfoCard alignment

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Sample from './Sample';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
