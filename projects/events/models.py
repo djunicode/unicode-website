c5dc030a81371869d5f60f3d60ade686c4df137b
Ananya Malik <40211791+ananyamalik@users.noreply.github.com>
2019-01-13 15:36:09 +0530

    Added Events App (#23)
    * Added Events App
    
    * Updated Events App
    
    * Updated events app
    
    * Updated Event app
    
    * Updated Event app

---
+++ projects/events/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from djangp.conf import settings
+from multiselectfield import MultiSelectField
+
+
+TECHNOLOGIES_CHOICES = (
+    ('web', 'Web Development'),
+    ('app', 'App Development'),
+)
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    technologies = MultiSelectField(choices=TECHNOLOGIES_CHOICES)
+    slug = models.SlugField(blank=True)
+    date = models.DateTimeField(auto_now=True)
+    event_amount = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
+class Participant(models.Model):
+    event = models.ForeignKey('Event', on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    sap_id = models.CharField(max_length=11)
+    email = models.EmailField(max_length=255, unique=True)
+    contact = models.CharField(max_length=10)
+
+    def __str__(self):
+        name = self.first_name + self.last_name
+        return name
