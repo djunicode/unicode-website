803248ed8c54f404da664f88ea27f21566817646
Harshil Jhaveri <36830810+harshiljhaveri@users.noreply.github.com>
2019-01-14 16:10:04 +0530

    Update views.py

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def news_view(request,*args,**kwargs):
+	obj=newslet.objects.get(id=1)
+	context = {
+	'email' : obj.email
+	}
+	
+	context={
+	'object': obj
+	}
+	return render(request,"newsletter.html",{})
