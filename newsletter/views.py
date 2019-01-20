67b7ffd6a4a92eefc7c92bbe56a71c1d34ab6ac7
Harshil Jhaveri <36830810+harshiljhaveri@users.noreply.github.com>
2019-01-20 09:10:33 +0530

    Update views.py

--- newsletter/views.py
+++ newsletter/views.py
@@ -9,10 +9,10 @@ from .models import newslet
 def news_view(request, *args, **kwargs):
         obj = newslet.objects.get(id=1)
         context = {
-                'email': obj.email
+        'email': obj.email
         }
 
         context = {
         'object': obj
         }
-	return render(request, "newsletter.html", context)
+        return render(request, "newsletter.html", context)
