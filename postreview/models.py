69d6ba6ddbe45f3d2d5fc23c1a21d159b4d14331
Harshil Jhaveri <36830810+harshiljhaveri@users.noreply.github.com>
2019-01-20 08:38:45 +0530

    Update models.py

--- postreview/models.py
+++ postreview/models.py
@@ -7,12 +7,10 @@ class review(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     photograph = models.ImageField(upload_to='review/%Y/%m/%d/', blank=True)
-    designation=models.CharField(max_length=128)
+    designation = models.CharField(max_length=128)
     company = models.CharField(max_length=128)
-    department=models.CharField(max_length=120)
+    department = models.CharField(max_length=120)
     comments = models.CharField(max_length=255)
     
     def __str__(self):
         return self.comments
-
-
