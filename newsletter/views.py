22ba63b17063dbcbb157db9e33f0199caa8ea434
Harshil Jhaveri <36830810+harshiljhaveri@users.noreply.github.com>
2019-01-20 08:37:29 +0530

    Update views.py

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,14 +1,17 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import get_user_model
+from django.core.paginator import Paginator
+from django.db.models import Q
 from .models import newslet
 
 def news_view(request, *args, **kwargs):
-	obj=newslet.objects.get(id=1)
-	context = {
-	'email' : obj.email
-	}
+        obj=newslet.objects.get(id=1)
+        context = {
+        'email' : obj.email
+        }
 
-	context={
-	'object': obj
-	}
+        context = {
+        'object': obj
+        }
 	return render(request, "newsletter.html", context)
