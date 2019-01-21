f292892b936ec58236eebab49d480f100b3b0e7b
Harshil Jhaveri <36830810+harshiljhaveri@users.noreply.github.com>
2019-01-21 20:58:08 +0530

    Update views.py

--- postreview/views.py
+++ postreview/views.py
@@ -3,11 +3,17 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import get_user_model
 from django.core.paginator import Paginator
 from django.db.models import Q
+from .forms import reviewForm
 from .models import review
 
 
 def create_review(request, *args, **kwargs):
+    form = reviewForm(request.review or None)
     obj = review.objects.get(id=1)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
     context = {
         "title": "Create",
         "first_name": obj.first_name,
