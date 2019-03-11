d6f33912cd4b0838b4fd5abab507dc6e32f96712
swapnil kadakia <swapnilkadakia@gmail.com>
2019-03-11 14:28:48 +0530

    first commit

---
+++ FrontEnd/src/scenes/Events/components/EventCards/EventCards2.js
@@ -0,0 +1,131 @@
+import React, { Component } from "react";
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  Avatar,
+  Grid
+} from "@material-ui/core";
+// import cardImg from "./images/cardImg.jpg";
+// import avatar from "./images/avatar.jpg";
+// import { styles } from "./styles/BlogCardStyle";
+import cardImg from "../../Images/cardImg.jpg";
+import PriceBtn from "../../../../components/buttons/priceButton/priceButton";
+
+class EventCards2 extends Component {
+  state = {};
+  styles = {
+    subFont: {
+      //   fontSize: 16,
+      //   fontFamily: "'Open Sans', sans-serif",
+      //   fontWeight: 200,
+      //   letterHeight: 22,
+      //   color: "hsla(0, 0%, 0%, 1)"
+      marginTop: "2%",
+      marginBottom: "0%"
+    },
+    card: {
+      boxShadow: "12px 12px 16px #d6d6d6",
+      maxWidth: 500,
+      borderRadius: "6px 6px 6px 0px",
+      margin: "auto"
+      //   marginBottom: "16%",
+      // margin: 10
+    },
+    boldFont: {
+      fontSize: 26,
+      fontFamily: "'Open Sans', sans-seri",
+      fontWeight: 600,
+      letterHeight: 36
+    },
+    boldFont2: {
+      fontSize: 16,
+      fontFamily: "'Open Sans', sans-seri !important",
+      fontWeight: 600,
+      letterHeight: 22
+    },
+    topFont: {
+      fontSize: 16,
+      fontFamily: "'Roboto', sans-serif !important",
+      fontWeight: 200,
+      letterHeight: "19 !important",
+      opacity: "1",
+      color: "#445DFF",
+      marginTop: "8px",
+      marginBottom: "8px",
+      letterSpacing: "1px"
+    },
+    dateFont: {
+      fontSize: 16,
+      fontFamily: "'Roboto', sans-serif !important ",
+      fontWeight: 400,
+      letterHeight: 19,
+      opacity: "1",
+      color: "hsla(0, 0%, 0%, 1)",
+      marginTop: "0px !important",
+      //   marginBottom: "8px",
+      letterSpacing: "2px",
+      position: "relative",
+      bottom: "19px"
+    },
+    dateInfo: {
+      fontSize: 16,
+      fontFamily: "'Roboto', sans-serif !important ",
+      fontWeight: 400,
+      letterHeight: 19,
+      opacity: "1",
+      color: "hsla(0, 0%, 0%, 1)",
+      marginTop: "0px !important",
+      //   marginBottom: "8px",
+      letterSpacing: "2px",
+      position: "relative",
+      bottom: "6px"
+    },
+
+    regFont: {
+      fontSize: 26,
+      fontFamily: "'Open Sans', sans-serif !important",
+      fontWeight: 600,
+      opacity: "1",
+      color: "rgba(68, 93, 255, 1)",
+      letterHeight: 36,
+      marginTop: "1%"
+    },
+    buttonPosition: {
+      marginLeft: "60% !important"
+    }
+  };
+  render() {
+    return (
+      <Card style={this.styles.card}>
+        <CardActionArea>
+          <CardMedia
+            style={{ height: 275 }}
+            image={cardImg}
+            title="Blog image"
+          />
+          <CardContent style={{ padding: "4% 2% 8% 5%" }}>
+            <div style={this.styles.topFont}>WEB DEVELOPMENT</div>
+
+            <Typography component="p" style={this.styles.regFont}>
+              What makes a great landing page?
+            </Typography>
+
+            <Typography variant="subtitle2" style={this.styles.subFont}>
+              Explore the design process behind some great landing page
+              examples. Lorem ipsum lorem ispum...
+            </Typography>
+            <PriceBtn price="50" />
+            <div style={this.styles.dateFont}>DATE:</div>
+            <div style={this.styles.dateInfo}>DEC 28, 2018</div>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    );
+  }
+}
+
+export default EventCards2;
