c35c962d58478f4fa209ecb757651125793b804e
Ananya Malik <ananyamalik1999@gmail.com>
2019-03-21 23:16:24 +0530

    Added API for events

--- website/urls.py
+++ website/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('projects/', include('projects.urls', namespace='projects')),
     path('api/projects/', include('projects.api.urls', namespace='projects-api')),
     path('events/', include('events.urls', namespace='events')),
+    path('api/events/', include('events.api.urls', namespace='events-api')),
 ]
 
 if settings.DEBUG:
