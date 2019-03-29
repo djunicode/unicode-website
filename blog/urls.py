8368b9160763fbcb3df3b46cbe703eb8b6290da2
Harshil Jhaveri <36830810+harshiljhaveri@users.noreply.github.com>
2019-03-29 14:31:06 +0530

    Added Blog View(#33)

--- blog/urls.py
+++ blog/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
-from postreview.views import (
-    post_create_view, post_update_view, list_reviews, review_detail_view,
-    review_delete_view
+from .views import (
+    post_list, post_detail, post_create, post_update, post_delete
 )
 
-app_name = 'Review'
+app_name = 'posts'
 
 urlpatterns = [
-    path('', list_reviews, name='product-list'),
-    path('create/', post_create_view, name='product-list'),
-    path('<int:id>/', review_detail_view, name='product-detail'),
-    path('<int:id>/update/', post_update_view, name='product-update'),
-    path('<int:id>/delete/', review_delete_view, name='product-delete'),
-    ]
+    path('', post_list, name='list'),
+    path('create', post_create, name='create'),
+    path('<slug:slug>', post_detail, name='detail'),
+    path('<slug:slug>/edit', post_update, name='edit'),
+    path('<slug:slug>/delete', post_delete, name='delete')
+]
