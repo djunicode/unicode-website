0fdd069b34f33044a040e534ed3770e57dd42b91
Aditya Jeswani <32266008+AdityaJ42@users.noreply.github.com>
2020-01-23 17:01:52 +0530

    Revert "Changes"

---
+++ FrontEndReact/src/scenes/Contacts/components/Image/styles/ImageStyles.js
@@ -0,0 +1,13 @@
+export const styles={
+    container:{
+        position: "relative",
+        textAlign: "centre",
+
+    },
+    centered: {
+        position: "absolute",
+        top: "80%",
+        left: "50%",
+        transform: "translate(-50%, -50%)" 
+    }
+};
